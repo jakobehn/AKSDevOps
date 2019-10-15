aab2d603ff4e4da24c2444be255517f96b146c32
Jakob Ehn <jakob@ehn.nu>
2019-10-16 10:57:15 +1100

    add delegation

---
+++ promitor/create-sp.ps1
@@ -0,0 +1,7 @@
+{
+    "appId": "c02b6d45-c92d-4da3-9bef-5aff601d3bf7",
+    "displayName": "azure-cli-2019-10-15-19-34-39",
+    "name": "http://azure-cli-2019-10-15-19-34-39",
+    "password": "eea00d02-f34f-4699-9d21-d1fad3a9c6ad",
+    "tenant": "7910c4f6-7843-49ce-b9de-dc9d4379f950"
+  }
