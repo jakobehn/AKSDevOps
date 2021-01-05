ba9eac1cff6a1057b0b6ced7c9d89568e9af0c1f
Jakob Ehn <jakob.ehn@mfex.com>
2021-01-05 09:31:22 +0100

    added new arm templates

---
+++ AKS/Network/deploy.ps1
@@ -0,0 +1,1 @@
+az deployment group create -g ndclondon --template-file .\network.json --parameters .\network.parameters.json
---
+++ AKS/deploy.ps1
@@ -0,0 +1,1 @@
+az deployment group create -g ndclondon --template-file .\cluster-new.json --parameters .\cluster-new.parameters.json
